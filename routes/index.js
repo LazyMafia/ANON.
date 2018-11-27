68dbfb6b42e5b2672d0c349ee0e22445fb9fdc6e
Eric Leus <ericleus1@gmail,com>
2018-11-27 10:08:49 -0500

    Added New Post Generation for Overview
    Added the algorithm to find posts that are new.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const router = express.Router();
 var postGenerationRequests = 0;
+var interestGenerationCalls = 0;
 var trendingGenerationCalls = 0;
+var newGenerationCalls = 0;
+var popularGenerationCalls = 0;
+var 
 var interestPercentage = 45;
 var userTrendingPercentage = 38;
 var trendingPercentage = 65;
 var userNewPercentage = 15;
 var newPercentage = 30;
 var posts = [];
+var interestPostsRaw = [];
+var interestPosts = [];
+var interestPostsValue = [];
 var trendingPostsRaw = [];
 var trendingPosts = [];
 var trendingPostsValue = [];
+var newPostsRaw = [];
+var newPosts = [];
+var newPostsValue = [];
+var popularPostsRaw = [];
+var popularPosts = [];
+var popularPostsValue = [];
 
 // Bring in User Model
 let User = require('../models/user');
@@ -93,49 +106,72 @@ function generateTrendingPost(){
                 }
             });
             
+            var postAlreadyChosen = false;
+
             trendingPostsRaw.forEach(function(post){
-                // Check if there are empty spaces in array that need to be filled
-                if(trendingPosts.length/postGenerationRequests < 20){
-                    trendingPosts.push(post);
-                } else{
-                    trendingPosts.forEach(function(existingPost){
-                        // Get value of post that is in trendingPost array
-                        var postValue = ((existingPost.favs*100) + existingPost.likes + existingPost.comments.length) / Date.now() - Date.parse(existingPost.create_date);
-                        trendingPostsValue.push(postValue);
-                    });
-
-                    // Get value of post that is being checked
-                    var postValue = ((post.favs*100) + post.likes + post.comments.length) / Date.now() - Date.parse(post.create_date);
-                    var trendingPostToBeReplaced = null;
-                    var i = 0;
-
-                    trendingPosts.forEach(function(existingPost){
-                        // Checks if the current post has a greater value than a post in trendingPosts
-                        if(postValue > trendingPostsValue[i]){
-                            // Checks if it is on the last post in the trendingPost array
-                            if(i+1 < trendingPosts.length){
-                                // If there is no other post that has a lesser value
-                                if(trendingPostToBeReplaced == null){
-                                    trendingPostToBeReplaced = i;
-                                } else {
-                                    // Checks if the existingPost has a lesser value than that of the one that was going to be replaced
-                                    if(trendingPostsValue[i] < trendingPostToBeReplaced){
-                                        trendingPostToBeReplaced = i;
+                // Checks if the post has already been chosen as a interesting, new or popular post
+                interestPosts.forEach(function(interestPost){
+                    if(post != interestPost){
+                        newPosts.forEach(function(newPost){
+                            if(post != newPost){
+                                popularPosts.forEach(function(popularPost){
+                                    if(post == popularPost){
+                                        postAlreadyChosen = true;
                                     }
-                                }
+                                });
                             } else{
-                                // Replaces a existingPost with the current post
-                                if(trendingPostToBeReplaced == null){
-                                    trendingPosts[trendingPostToBeReplaced] = post;
-                                } else {
-                                    if(trendingPostsValue[i] < trendingPostToBeReplaced){
-                                        trendingPosts[i] = post;
+                                postAlreadyChosen = true;
+                            }
+                        });
+                    } else{
+                        postAlreadyChosen = true;
+                    }
+                });
+
+                if(!postAlreadyChosen){
+                    // Check if there are empty spaces in array that need to be filled
+                    if(trendingPosts.length/postGenerationRequests < 20){
+                        trendingPosts.push(post);
+                    } else{
+                        trendingPosts.forEach(function(existingPost){
+                            // Get value of post that is in trendingPost array
+                            var postValue = ((existingPost.favs*100) + existingPost.likes + existingPost.comments.length) / Date.now() - Date.parse(existingPost.create_date);
+                            trendingPostsValue.push(postValue);
+                        });
+
+                        // Get value of post that is being checked
+                        var postValue = ((post.favs*100) + post.likes + post.comments.length) / Date.now() - Date.parse(post.create_date);
+                        var trendingPostToBeReplaced = null;
+                        var i = 0;
+
+                        trendingPosts.forEach(function(existingPost){
+                            // Checks if the current post has a greater value than a post in trendingPosts
+                            if(postValue > trendingPostsValue[i]){
+                                // Checks if it is on the last post in the trendingPost array
+                                if(i+1 < trendingPosts.length){
+                                    // If there is no other post that has a lesser value
+                                    if(trendingPostToBeReplaced == null){
+                                        trendingPostToBeReplaced = i;
+                                    } else {
+                                        // Checks if the existingPost has a lesser value than that of the one that was going to be replaced
+                                        if(trendingPostsValue[i] < trendingPostToBeReplaced){
+                                            trendingPostToBeReplaced = i;
+                                        }
+                                    }
+                                } else{
+                                    // Replaces a existingPost with the current post
+                                    if(trendingPostToBeReplaced == null){
+                                        trendingPosts[trendingPostToBeReplaced] = post;
+                                    } else {
+                                        if(trendingPostsValue[i] < trendingPostToBeReplaced){
+                                            trendingPosts[i] = post;
+                                        }
                                     }
                                 }
-                            }
-                        } 
-                        i++;
-                    });
+                            } 
+                            i++;
+                        });
+                    }
                 }
             });
             // Rearrange trendingPosts in order of value
@@ -145,7 +181,7 @@ function generateTrendingPost(){
             trendingPostsRaw = [];
         });
     } else {
-        // Send the trending post to posts array
+        // Push the trending post to posts array
         posts.push(trendingPosts[trendingGenerationCalls]);
     }
     trendingGenerationCalls++;
@@ -153,6 +189,99 @@ function generateTrendingPost(){
 
 function generateNewPost(){
     console.log('New');
+    // DISCLAIMER: Category is being used to test instead of Post due to current inability to sign in and add new posts
+    // HOW TO CHANGE TO POST:
+    // - Category.find() to Post.find()
+    // - post.create_date to post.post_date
+    if(newPosts.length/postGenerationRequests - newGenerationCalls <= 8){
+        // Find all posts
+        Category.find({}, function(err, posts){
+            posts.forEach(function(post){
+                // Find all posts that are less than 1 day old
+                if((Date.now()-Date.parse(post.create_date))/(1000*3600*24) < 1){
+                    newPostsRaw.push(post);
+                }
+            });
+            
+            var postAlreadyChosen = false;
+
+            newPostsRaw.forEach(function(post){
+                // Checks if the post has already been chosen as a interesting, trending or popular post
+                interestPosts.forEach(function(interestPost){
+                    if(post != interestPost){
+                        trendingPosts.forEach(function(trendingPost){
+                            if(post != trendingPost){
+                                popularPosts.forEach(function(popularPost){
+                                    if(post == popularPost){
+                                        postAlreadyChosen = true;
+                                    }
+                                });
+                            } else{
+                                postAlreadyChosen = true;
+                            }
+                        });
+                    } else{
+                        postAlreadyChosen = true;
+                    }
+                });
+
+                if(!postAlreadyChosen){
+                    // Check if there are empty spaces in array that need to be filled
+                    if(newPosts.length/postGenerationRequests < 16){
+                        newPosts.push(post);
+                    } else{
+                        newPosts.forEach(function(existingPost){
+                            // Get value of post that is in newPost array
+                            var postValue = (existingPost.favs*100) + existingPost.likes + existingPost.comments.length;
+                            newPostsValue.push(postValue);
+                        });
+
+                        // Get value of post that is being checked
+                        var postValue = (post.favs*100) + post.likes + post.comments.length;
+                        var newPostToBeReplaced = null;
+                        var i = 0;
+
+                        newPosts.forEach(function(existingPost){
+                            // Checks if the current post has a greater value than a post in newPosts
+                            if(postValue > newPostsValue[i]){
+                                // Checks if it is on the last post in the newPost array
+                                if(i+1 < newPosts.length){
+                                    // If there is no other post that has a lesser value
+                                    if(newPostToBeReplaced == null){
+                                        newPostToBeReplaced = i;
+                                    } else {
+                                        // Checks if the existingPost has a lesser value than that of the one that was going to be replaced
+                                        if(newPostsValue[i] < newPostToBeReplaced){
+                                            newPostToBeReplaced = i;
+                                        }
+                                    }
+                                } else{
+                                    // Replaces a existingPost with the current post
+                                    if(newPostToBeReplaced == null){
+                                        newPosts[newPostToBeReplaced] = post;
+                                    } else {
+                                        if(newPostsValue[i] < newPostToBeReplaced){
+                                            newPosts[i] = post;
+                                        }
+                                    }
+                                }
+                            } 
+                            i++;
+                        });
+                    }
+                }
+            });
+            // Rearrange newPosts in order of value
+            newPosts.sort(function(a, b){return b-a});
+            // Variable Resets
+            newPostsValue = [];
+            newPostsRaw = [];
+        });
+    } else {
+        // Push the new post to posts array
+        posts.push(newPosts[newGenerationCalls]);
+    }
+    newGenerationCalls++;
 }
 
 function generatePopularPost(){
