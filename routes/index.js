1c3864f67c218ac5567f9ef91bfc22e141e15a67
Eric Leus <ericleus1@gmail,com>
2018-11-26 23:52:18 -0500

    Setup Trending Post Generation
    When trending posts are called to be generated, the algorithm checks to see if there are left over trending posts in an array. If there are no more, 20 more are generated and are pushed to an array of posts that are trending. Each time a trending post is randomly chosen to be shown, the ones that are trending the most are pushed to a posts array. The post array is for all posts that are to be showed on the client side HTML page.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 var postGenerationRequests = 0;
+var trendingGenerationCalls = 0;
 var interestPercentage = 45;
 var userTrendingPercentage = 38;
 var trendingPercentage = 65;
@@ -9,6 +10,7 @@ var newPercentage = 30;
 var posts = [];
 var trendingPostsRaw = [];
 var trendingPosts = [];
+var trendingPostsValue = [];
 
 // Bring in User Model
 let User = require('../models/user');
@@ -77,24 +79,76 @@ function generateInterestsPost(){
 
 function generateTrendingPost(){
     console.log('Trending');
-    Category.find({}, function(err, posts){
-        posts.forEach(function(post){
-            // Find all posts that are less than 7 days old
-            if((Date.now()-Date.parse(post.create_date))/(1000*3600*24) < 7){
-                trendingPostsRaw.push(post);
-            } else{
-                console.log("More than 7 days old");
-            }
-        });
+    // DISCLAIMER: Category is being used to test instead of Post due to current inability to sign in and add new posts
+    // HOW TO CHANGE TO POST:
+    // - Category.find() to Post.find()
+    // - post.create_date to post.post_date
+    if(trendingPosts.length/postGenerationRequests - trendingGenerationCalls <= 10){
+        // Find all posts
+        Category.find({}, function(err, posts){
+            posts.forEach(function(post){
+                // Find all posts that are less than 7 days old
+                if((Date.now()-Date.parse(post.create_date))/(1000*3600*24) < 7){
+                    trendingPostsRaw.push(post);
+                }
+            });
+            
+            trendingPostsRaw.forEach(function(post){
+                // Check if there are empty spaces in array that need to be filled
+                if(trendingPosts.length/postGenerationRequests < 20){
+                    trendingPosts.push(post);
+                } else{
+                    trendingPosts.forEach(function(existingPost){
+                        // Get value of post that is in trendingPost array
+                        var postValue = ((existingPost.favs*100) + existingPost.likes + existingPost.comments.length) / Date.now() - Date.parse(existingPost.create_date);
+                        trendingPostsValue.push(postValue);
+                    });
 
-        trendingPostsRaw.forEach(function(post){
-            if(trendingPosts.length/postGenerationRequests < 15){
-                trendingPosts.push(post);
-            } else{
+                    // Get value of post that is being checked
+                    var postValue = ((post.favs*100) + post.likes + post.comments.length) / Date.now() - Date.parse(post.create_date);
+                    var trendingPostToBeReplaced = null;
+                    var i = 0;
 
-            }
+                    trendingPosts.forEach(function(existingPost){
+                        // Checks if the current post has a greater value than a post in trendingPosts
+                        if(postValue > trendingPostsValue[i]){
+                            // Checks if it is on the last post in the trendingPost array
+                            if(i+1 < trendingPosts.length){
+                                // If there is no other post that has a lesser value
+                                if(trendingPostToBeReplaced == null){
+                                    trendingPostToBeReplaced = i;
+                                } else {
+                                    // Checks if the existingPost has a lesser value than that of the one that was going to be replaced
+                                    if(trendingPostsValue[i] < trendingPostToBeReplaced){
+                                        trendingPostToBeReplaced = i;
+                                    }
+                                }
+                            } else{
+                                // Replaces a existingPost with the current post
+                                if(trendingPostToBeReplaced == null){
+                                    trendingPosts[trendingPostToBeReplaced] = post;
+                                } else {
+                                    if(trendingPostsValue[i] < trendingPostToBeReplaced){
+                                        trendingPosts[i] = post;
+                                    }
+                                }
+                            }
+                        } 
+                        i++;
+                    });
+                }
+            });
+            // Rearrange trendingPosts in order of value
+            trendingPosts.sort(function(a, b){return b-a});
+            // Variable Resets
+            trendingPostsValue = [];
+            trendingPostsRaw = [];
         });
-    });
+    } else {
+        // Send the trending post to posts array
+        posts.push(trendingPosts[trendingGenerationCalls]);
+    }
+    trendingGenerationCalls++;
 }
 
 function generateNewPost(){
